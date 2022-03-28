a7ebc415860c9165c22d29ae838a9429d719f700
kapitalisman <sam.oosterveer@gmail.com>
2022-03-28 20:22:28 +0200

    added authors

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
+// environment variables
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
+// require packages
 const express = require('express')
-const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
-const indexRouter = require('./routes/index')
-
+// create app
+const app = express()
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
 
+// use packages
+app.use(express.static('public'))
+app.use(express.urlencoded({ limit: '10mb', extended: false }));
+app.use(expressLayouts)
+
+// database
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true
@@ -22,6 +27,11 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+// blueprints
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
